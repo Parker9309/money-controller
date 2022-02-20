8e3a4a764c6d02d6689eda6e64d51675951bb1c0
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-02-20 00:36:06 -0300

    feat: styled components explained

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import styled from 'styled-components';
+
+const Tittle = styled.h1`
+  color: red;
+
+// o styled components suporta encadeamento de tags assim como o SASS
+
+  input {
+    background-color: black;
+  }
+  `
+
 export function App() {
   return (
     <div className="App">
-      <p>Fala fiote</p>
+      <Tittle>
+        Fala fiote
+        <input type="text" />
+      </Tittle>
     </div>
   );
 }
